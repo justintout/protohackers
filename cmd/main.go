c2cc77bccf7b820fe0a44396dd9b8017a61f769d
justintout <3639630+justintout@users.noreply.github.com>
2022-12-01 02:59:01 -0500

    solve 1

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/justintout/protohackers"
 )
 
+// TODO: shutdown is sketchy and leaks. needs channels.
 func main() {
 	fmt.Printf("starting problem 0 server: %s\n", protohackers.Addr0)
 	s0 := protohackers.EchoServer{}
-	s0.ListenAndServe(protohackers.Addr0)
+	go s0.ListenAndServe(protohackers.Addr0)
+
+	fmt.Printf("starting problem 1 server: %s\n", protohackers.Addr1)
+	s1 := protohackers.PrimeServer{}
+	go s1.ListenAndServe(protohackers.Addr1)
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	<-done
+	s0.Close()
+	s1.Close()
 }
